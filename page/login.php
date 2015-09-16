0a68d5862694019515e87e55e59da3a5f6852814
Hendrik Vallimägi <hendval@greeny.cs.tlu.ee>
2015-09-16 12:47:28 +0300

    lol

--- page/login.php
+++ page/login.php
@@ -10,25 +10,31 @@
 		
 		//echo "keegi vajutas nuppu";
 		
+		//vajutas login nuppu
+		if(isset($_POST["login"])){
 		
-		if(empty($_POST["email"]))  {
-			$email_error = "Email on kohustuslik!";
-			
-			
-		}
-		
-		if(empty($_POST["password"])) {
-			$password_error = "Parool on kohustuslik!";
-		} else {
-			
-			//kui oleme siia jõudnud siis parool ei ole tühi
-			//kontrollin et olek vähemalt 8 sümbolit pikk
-			if(strlen($_POST["password"]) < 8) {
+			if(empty($_POST["email"]))  {
+				$email_error = "Email on kohustuslik!";
 				
-				$password_error = "Parool peab olema vähemalt 8 tähemärki pikk!";
 				
 			}
+			
+			if(empty($_POST["password"])) {
+				$password_error = "Parool on kohustuslik!";
+			} else {
+				
+				//kui oleme siia jõudnud siis parool ei ole tühi
+				//kontrollin et olek vähemalt 8 sümbolit pikk
+				if(strlen($_POST["password"]) < 8) {
+					
+					$password_error = "Parool peab olema vähemalt 8 tähemärki pikk!";
+					
+				}
+			}
+			
 		}
+		
+		
 		
 	}
 ?>
@@ -49,7 +55,7 @@
 				<input name="email" type="email" placeholder="E-post"><br>
 				Parool:<br>
 				<input name="password" type="password" placeholder="Parool"><br><br>
-				<input type="submit" value="Logi sisse"><br><br>
+				<input name="login" type="submit" value="Logi sisse"><br><br>
 				<?php echo $password_error; ?>
 			<?php echo $email_error; ?>
 			</form>
--- page/register.php
+++ page/register.php
@@ -11,23 +11,30 @@
 		
 		//echo "keegi vajutas nuppu";
 		
+		//vajutas register nuppu
 		
-		if(empty($_POST["email"]))  {
-			$email_error = "VEATEADE: Email on kohustuslik!";
+		if(isset($_POST["register"])) {
+			
+			if(empty($_POST["email"]))  {
+				$email_error = "VEATEADE: Email on kohustuslik!";
+			}
+			
+			if(empty($_POST["password"])) {
+				$password_error = "VEATEADE: Parool on kohustuslik!";
+			} else {
+				//kui oleme siia jõudnud siis parool ei ole tühi
+				//kontrollin et olek vähemalt 8 sümbolit pikk
+				if(strlen($_POST["password"]) < 8) {
+					$password_error = "VEATEADE: Parool peab olema vähemalt 8 tähemärki pikk!";
+				}
+				if($_POST["password"] != $_POST["password_repeat"]) {
+					$password_repeat_error = "VEATEADE: Paroolid peavad kattuma!";
+				}
+			}
+			
 		}
 		
-		if(empty($_POST["password"])) {
-			$password_error = "VEATEADE: Parool on kohustuslik!";
-		} else {
-			//kui oleme siia jõudnud siis parool ei ole tühi
-			//kontrollin et olek vähemalt 8 sümbolit pikk
-			if(strlen($_POST["password"]) < 8) {
-				$password_error = "VEATEADE: Parool peab olema vähemalt 8 tähemärki pikk!";
-			}
-			if($_POST["password"] != $_POST["password_repeat"]) {
-				$password_repeat_error = "VEATEADE: Paroolid peavad kattuma!";
-			}
-		}
+		
 		
 		
 	}
@@ -57,7 +64,7 @@
 				<input name="password" type="password" placeholder="Parool"><br>
 				Parool uuesti:<br>
 				<input name="password_repeat" type="password" placeholder="Parool uuesti"><br><br>
-				<input type="submit" value="Registreeri"><br><br>
+				<input name="register" type="submit" value="Registreeri"><br><br>
 				<?php echo $password_repeat_error; ?><?php echo $password_error; ?><?php echo $email_error; ?>
 			</form>
 <a href="login.php">Kasutaja olemas? Logi sisse siin!</a>
